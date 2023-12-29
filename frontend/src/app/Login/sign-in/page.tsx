ffbda6dd2d8c1556904273df65d2ff3837cb7ed5
Gabor Adorjani <gadorjani@windowslive.com>
2023-12-29 16:23:40 +0000

    Update page.tsx

--- frontend/src/app/Login/sign-in/page.tsx
+++ frontend/src/app/Login/sign-in/page.tsx
@@ -9,10 +9,10 @@ const Login = () => {
   return (
     <div className='relaitve flex h-full flex-row'>
       
-        <div className='basis-2/3 bg-gradient-to-r from-[#602ee4] to-[#14a0eb]'>
+        <div className='md:basis-2/3 hidden  bg-gradient-to-r from-[#602ee4] to-[#14a0eb]'>
           <Icon name='shape'/>
         </div>
-        <div className='basis-1/3 flex flex-col justify-center items-center gap-2 py-5 px-5 bg-gradient-to-r from-[#66a6db] to-[#c9d7e7]'>
+        <div className='md:basis-1/3 w-full flex flex-col justify-center items-center gap-2 py-5 px-5 bg-gradient-to-r from-[#66a6db] to-[#c9d7e7]'>
           <div className='mt-5 mb-5 '>Welcome to Hogwarts</div>
           <InputField/>
           <div>
