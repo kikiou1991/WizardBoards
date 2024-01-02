e6f489424963d2b38e29da7a9640a285a3f51a9e
Gabor Adorjani <gadorjani@windowslive.com>
2024-01-02 16:58:34 +0000

    user profile correction

--- frontend/src/app/Main/components/navbar/profile.tsx
+++ frontend/src/app/Main/components/navbar/profile.tsx
@@ -3,51 +3,52 @@ import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, Dropdown
 import React from 'react'
 
 interface Props {
-    name: string,
-    location: string,
-    email: string
+  name: string,
+  location: string,
+  email: string
 }
 
-const Profile = ({name, location, email}: Props) => {
+const Profile = ({ name, location, email }: Props) => {
   return (
     <Navbar className='bg-inherit'>
-    
-          <NavbarContent as="div" className='items-center'>
-              <Dropdown className='bg-[#041A42]' placement='bottom-end'>
-                <DropdownTrigger>
-                  <Avatar
-                          isBordered
-                          as="button"
-                          className="transition-transform"
-                          color="secondary"
-                          name={name}
-                          size="sm"
-                          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                    />
 
-                </DropdownTrigger>
-                <DropdownMenu >
-                  <DropdownSection className='text-[#E5EAF3]'>
-                    <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
-                      <p className="font-semibold">Signed in as</p>
-                      <p className="font-semibold">{email}</p></DropdownItem>
-                    <DropdownItem className='data-[hover=true]:bg-[#143F88]'>{name}</DropdownItem>
-                    <DropdownItem className='data-[hover=true]:bg-[#143F88]'>{location}</DropdownItem>
-                    <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
-                      <p className=''>Settings</p>
-                    </DropdownItem>
-                    <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
-                      <p className=''>Logout</p>
-                    </DropdownItem>
-
-
-                  </DropdownSection>
-                  
-                </DropdownMenu>
-              </Dropdown>
-          </NavbarContent>
+      <NavbarContent as="div" className='items-center'>
+        <Dropdown className='bg-[#041A42]' placement='bottom-end'>
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name={name}
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownSection className='text-[#E5EAF3]'>
+              <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{email}</p>
+              </DropdownItem>
+              <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
+                <p className='font-semibold'>{name}</p>
+              </DropdownItem>
+              <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
+                <p className='font-semibold'>{location}</p>
+              </DropdownItem>
+              <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
+                <p className='font-semibold'>Settings</p>
+              </DropdownItem>
+              <DropdownItem className='data-[hover=true]:bg-[#143F88]'>
+                <p className='font-semibold'>Logout</p>
+              </DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
     </Navbar>
   )
 }
 
-export default Profile
+export default Profile;
