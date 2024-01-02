55a168906684800470d4abdf4dea2e38e6b6e275
Gabor Adorjani <gadorjani@windowslive.com>
2024-01-02 14:57:59 +0000

    backend

--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { Signup, Login } = require('../controllers/authcontroller');
 const { userVerification } = require('../middlewares/authmiddleware');
 const { CreateWorkspace, GetUserWorkspace } = require('../controllers/createworkspace.js');
+const { GetUserById, GetAllUsers } = require('../controllers/usercontrollers.js')
 
 router.post("/api/signup", Signup);
 router.post("/api/login", Login);
@@ -12,5 +13,9 @@ router.post("/api/", userVerification);
 // Workspace routes
 router.get("/api/workspaces", userVerification, GetUserWorkspace);
 router.post("/api/workspaces", userVerification, CreateWorkspace);
+
+//Get user by ID/all users
+router.get("/api/users/:userId", userVerification, GetUserById);
+router.get("/api/users", userVerification, GetAllUsers);
 
 module.exports = router;
