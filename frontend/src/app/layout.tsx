a42cf7d35c91917cf6425dc6ad39e0f030cf1d33
Gabor Adorjani <gadorjani@windowslive.com>
2024-01-03 11:50:19 +0000

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,23 +1,28 @@
 // app/layout.tsx
-import {UserContextProvider} from '@/contexts/Usercontext';
-import {Playfair_Display} from 'next/font/google';
-import './globals.css';
-import {Providers} from './providers';
+import {Providers} from "./providers";
+
+import "./globals.css";
+import {Playfair_Display} from 'next/font/google'
+import { UserContextProvider } from "@/contexts/Usercontext";
 
 const playFair = Playfair_Display({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
-});
+})
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
-    <html lang='en' className='dark'>
-      <body className={playFair.className}>
+    <html lang="en" className='dark'>
+      <body 
+      className={playFair.className}>
         <UserContextProvider>
-          <Providers>{children}</Providers>
+          <Providers>
+            {children}
+          </Providers>
         </UserContextProvider>
       </body>
     </html>
   );
 }
+
