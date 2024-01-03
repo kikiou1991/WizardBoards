6c7df0449018f53cb9b363bae23a6af8562ae618
Szabó Gergő <szabogergo20011008@gmail.com>
2024-01-03 12:19:05 +0100

    help gabinak

--- frontend/src/app/workspace/home/page.tsx
+++ frontend/src/app/workspace/home/page.tsx
@@ -1,20 +1,8 @@
-import NavbarTop from '@/components/navbar/navbar';
-import Sidebar from '@/components/sidebar/sidebar';
 import Project from '../projects/page';
 const Home = () => {
   return (
     <div className='flex flex-col h-full'>
-      <div className='w-full'>
-        <NavbarTop />
-      </div>
-      <div className=' w-full  flex flex-row flex-grow'>
-        <div className='w-1/6 h-full'>
-          <Sidebar />
-        </div>
-        <div className='w-5/6 h-full'>
-          <Project />
-        </div>
-      </div>
+      <Project />
     </div>
   );
 };
---
+++ frontend/src/app/workspace/layout.tsx
@@ -0,0 +1,29 @@
+import NavbarTop from '@/components/navbar/navbar';
+import Sidebar from '@/components/sidebar/sidebar';
+
+type LayoutProps = {
+  title?: string;
+  children: React.ReactNode;
+};
+export default function WorkspaceLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: {
+    tag: string;
+    item: string;
+  };
+}) {
+  return (
+    <div className='flex h-screen max-h-screen flex-col overflow-hidden'>
+      <NavbarTop />
+      <div className='flex h-fit grow  flex-row overflow-hidden '>
+        <div className='w-1/6 h-full'>
+          <Sidebar />
+        </div>
+        <div className='w-5/6 h-full'>{children}</div>
+      </div>
+    </div>
+  );
+}
