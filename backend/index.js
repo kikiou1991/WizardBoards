c4a6fd61f5a7da0b602d0c780e38f476b3f8331b
Szabó Gergő <szabogergo20011008@gmail.com>
2024-01-03 11:50:53 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,15 @@ mongoose
     console.error(error);
   });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
