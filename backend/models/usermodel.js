bd00431ffe0e9032284186527b9103c499adcb3d
Gabor Adorjani <gadorjani@windowslive.com>
2024-01-03 13:23:52 +0000

    hashing bug fix

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -30,10 +30,5 @@ const userSchema = new mongoose.Schema({
     ],
 });
 
-userSchema.pre("save", async function () {
-    const bcrypt = require('bcrypt');
-
-    this.password = await bcrypt.hash(this.password, 12);
-})
 
 module.exports = mongoose.model("User", userSchema);
