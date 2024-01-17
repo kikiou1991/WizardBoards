75b48292d36330d381cf111ef8c474f3362742ff
Gabor Adorjani <gadorjani@windowslive.com>
2024-01-17 20:43:24 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/wizardboards",
+    images: {
+        remotePatterns: [{
+            protocol: "https",
+            hostname:'media.istockphoto.com',
+           
+        }
+        ]
+    }
 }
 
 module.exports = nextConfig
