6e4364e5821b8a7e6235d91a75589b4780a702dc
kikiou91 <gadorjani@windowslive.com>
2024-01-27 16:09:17 +0000

    ssr error fix??

--- frontend/src/app/workspace/boardview/page.tsx
+++ frontend/src/app/workspace/boardview/page.tsx
@@ -1,10 +1,45 @@
-import { useContext, useEffect } from 'react';
-import { UserContext, UserContextType } from '@/contexts/Usercontext';
+import { useEffect, useState } from 'react';
+import { workspaceBoards } from '@/lib/boards';
+import { userWorkspaces } from '@/lib/workspaces';
 
 const BoardView = () => {
-  const { boards } = useContext(UserContext) as UserContextType;
+  const [workspaces, setWorkspaces] = useState<any[]>([]);
+  const [boards, setBoards] = useState<any[]>([]);
 
-  useEffect(() => {}, [boards]);
+  const fetchWorkspaces = async (token: any) => {
+    if (!token) {
+      console.log('Token is missing');
+    }
+
+    try {
+      let res = await userWorkspaces.fetchWorkspaces(token);
+
+      setWorkspaces(res?.data || []);
+    } catch (error: any) {
+      // Handle error if needed
+      console.error('Error fetching workspaces:', error || error.message || error.response);
+    }
+  };
+  const fetchBoard = async (token: any, workspaceUuid: string) => {
+    try {
+      const res = await workspaceBoards.fetchBoard(token, workspaceUuid);
+      setBoards(res?.data || []);
+    } catch (error) {
+      console.error('Failed to fetch boards', error);
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    fetchWorkspaces(token);
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (workspaces.length !== 0) {
+      fetchBoard(token, workspaces[0].uuid);
+    }
+  }, [workspaces]);
 
   return (
     <div className='grow overflow-hidden flex flex-col overflow-x-auto overflow-y-hidden  whitespace-nowrap'>
