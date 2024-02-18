22095028cef0b26d780d7dc70d1ae9ffa95d9400
Szabó Gergő <szabogergo20011008@gmail.com>
2024-02-18 02:42:02 +0100

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 // app/layout.tsx
-import { Providers } from './providers';
+import {Providers} from './providers';
 
-import { UserContextProvider } from '@/contexts/Usercontext';
-import { Playfair_Display } from 'next/font/google';
+import {UserContextProvider} from '@/contexts/Usercontext';
+import {Playfair_Display} from 'next/font/google';
 import './globals.css';
 
 const playFair = Playfair_Display({
@@ -10,8 +10,16 @@ const playFair = Playfair_Display({
   subsets: ['latin'],
   display: 'swap',
 });
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: 'WizardBoards - See the future',
+  icons: {
+    icon: 'https://barsoft.hu/ipanel/favicon.ico',
+    shortcut: 'https://barsoft.hu/ipanel/favicon-32x32.png',
+    apple: 'https://barsoft.hu/ipanel/apple-touch-icon.png',
+  },
+  description: 'WizardBoards for those who can see the future.',
+};
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en' suppressHydrationWarning={true}>
       <body className={playFair.className}>
