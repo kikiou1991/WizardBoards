dd58e129404e2e6421b7e7f15f21e81c9511e8d4
Szabó Gergő <szabogergo20011008@gmail.com>
2024-02-18 02:34:59 +0100

    aria fuck

--- frontend/src/components/navbar/profile.tsx
+++ frontend/src/components/navbar/profile.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { UserContext, UserContextType } from '@/contexts/Usercontext';
-import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Navbar, NavbarContent } from '@nextui-org/react';
-import { useContext } from 'react';
+import {UserContext, UserContextType} from '@/contexts/Usercontext';
+import {Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Navbar, NavbarContent} from '@nextui-org/react';
+import {useContext} from 'react';
 import Socials from './socials';
 
 interface Props {
@@ -10,17 +10,17 @@ interface Props {
   email: string;
 }
 
-const Profile = ({ name, email }: Props) => {
-  const { handleLogout } = useContext(UserContext) as UserContextType;
+const Profile = ({name, email}: Props) => {
+  const {handleLogout} = useContext(UserContext) as UserContextType;
   return (
-    <Navbar className='bg-background px-1' classNames={{ wrapper: 'px-0' }}>
+    <Navbar className='bg-background px-1' classNames={{wrapper: 'px-0'}}>
       <NavbarContent as='div' className='items-center'>
-        <Dropdown className='text-foreground' placement='bottom-end'>
-          <DropdownTrigger>
+        <Dropdown className='text-foreground' placement='bottom-end' aria-label='aria fuck'>
+          <DropdownTrigger aria-label='aria fuck'>
             <Avatar isBordered as='button' className='transition-transform p' color='primary' name={name} size='sm' src='https://i.pravatar.cc/150?u=a042581f4e29026704d' />
           </DropdownTrigger>
-          <DropdownMenu>
-            <DropdownSection>
+          <DropdownMenu aria-label='aria fuck'>
+            <DropdownSection aria-label='aria fuck'>
               <DropdownItem>
                 <p className='py-0 px-0'>Account</p>
                 <div className='sm:hidden'>
@@ -28,26 +28,26 @@ const Profile = ({ name, email }: Props) => {
                 </div>
               </DropdownItem>
             </DropdownSection>
-            <DropdownSection className='text-current' showDivider>
-              <DropdownItem className='hover:bg-secondaryBG'>
+            <DropdownSection className='text-current' showDivider aria-label='aria fuck'>
+              <DropdownItem className='hover:bg-secondaryBG' aria-label='aria fuck'>
                 <p className='font-semibold'>Signed in as</p>
                 <p className='font-semibold p-0'>{email}</p>
               </DropdownItem>
-              <DropdownItem></DropdownItem>
-              <DropdownItem className='hover:bg-secondaryBG'>
+              <DropdownItem aria-label='aria fuck'></DropdownItem>
+              <DropdownItem className='hover:bg-secondaryBG' aria-label='aria fuck'>
                 <p className='font-semibold p-0'>{name}</p>
               </DropdownItem>
             </DropdownSection>
-            <DropdownSection showDivider>
-              <DropdownItem className='hover:bg-secondaryBG'>
+            <DropdownSection showDivider aria-label='aria fuck'>
+              <DropdownItem className='hover:bg-secondaryBG' aria-label='aria fuck'>
                 <p className='font-semibold'>Help, feedback</p>
               </DropdownItem>
-              <DropdownItem className='hover:bg-secondaryBG'>
+              <DropdownItem className='hover:bg-secondaryBG' aria-label='aria fuck'>
                 <p className='font-semibold'>Settings</p>
               </DropdownItem>
             </DropdownSection>
-            <DropdownSection>
-              <DropdownItem className='hover:bg-secondaryBG flex flex-row' onClick={handleLogout}>
+            <DropdownSection aria-label='aria fuck'>
+              <DropdownItem className='hover:bg-secondaryBG flex flex-row' onClick={handleLogout} aria-label='aria fuck'>
                 <p className='font-semibold'>Logout</p>
               </DropdownItem>
             </DropdownSection>
