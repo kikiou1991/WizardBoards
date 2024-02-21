1390f40b316684ad5a455b1ed93c15e9447a7dc2
Szabó Gergő <szabogergo20011008@gmail.com>
2024-02-21 14:32:05 +0100

    Update index.js

--- backend2/index.js
+++ backend2/index.js
@@ -28,9 +28,17 @@ app.use((err, req, res, next) => {
   res.status(500).send('Internal Server Error');
 });
 const mongoClient = new MongoClient(uri);
+const excludedPaths = ['/api/v2/login', '/api/v2/register'];
 
 const checkTokenMiddleware = (req, res, next) => {
   const token = req.headers?.authorization?.split(' ')[1];
+  const path = req.baseUrl;
+  console.log('path:', path);
+
+  // Check if the path is in the excluded paths list
+  if (excludedPaths.includes(path)) {
+    return next(); // Skip token verification for excluded paths
+  }
   if (!token) {
     return res.status(401).json({message: 'Unauthorized: No token provided'});
   } else {
@@ -66,7 +74,6 @@ app.use(
   },
   checkTokenMiddleware
 );
-
 startServer();
 
 async function startServer() {
