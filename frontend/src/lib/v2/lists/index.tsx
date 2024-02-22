67423ceca54cd833476cffe26ad8d03ffb5ab9ad
kikiou91 <gadorjani@windowslive.com>
2024-02-22 09:48:31 +0000

    get:List api call fix

--- frontend/src/lib/v2/lists/index.tsx
+++ frontend/src/lib/v2/lists/index.tsx
@@ -1,34 +1,33 @@
 export const boardLists = {
   getLists: async (boardUuid: string, data: any, token: any) => {
     try {
-      const response = await fetch(`https://wizardboards.co.uk/api/v2/lists`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          boardUuid,
-          data,
-        }),
-      });
+      const response = await fetch(
+        `https://wizardboards.co.uk/api/v2/lists?boardUuid=${boardUuid}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
       if (!response.ok) {
-        throw new Error('Failed to fetch lists');
+        throw new Error("Failed to fetch lists");
       }
 
       const newList = await response.json();
       return { newList };
     } catch (error: any) {
-      console.error('Error while trying to fetch lists: ', error.message);
+      console.error("Error while trying to fetch lists: ", error.message);
     }
   },
   createList: async (token: any, listData: any, boardUuid: string) => {
     try {
-      const response = await fetch('https://wizardbioards.co.uk/api/v2/lists', {
-        method: 'POST',
+      const response = await fetch("https://wizardbioards.co.uk/api/v2/lists", {
+        method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           boardUuid,
@@ -37,37 +36,40 @@ export const boardLists = {
         }),
       });
       if (!response.ok) {
-        throw new Error('Failed to create list');
+        throw new Error("Failed to create list");
       }
 
       const newList = await response.json();
       return { newList };
     } catch (error: any) {
-      console.error('Error creating list: ', error.message);
+      console.error("Error creating list: ", error.message);
     }
   },
   deleteList: async (token: any, boardUuid: string, listData: any) => {
     try {
-      const response = await fetch('https://wizardboards.co.uk/api/v2/lists/archive', {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          boardUuid,
-          listUUID: listData.uuid,
-          data: listData,
-        }),
-      });
+      const response = await fetch(
+        "https://wizardboards.co.uk/api/v2/lists/archive",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            boardUuid,
+            listUUID: listData.uuid,
+            data: listData,
+          }),
+        }
+      );
       if (!response.ok) {
-        throw new Error('Failed to delete the list');
+        throw new Error("Failed to delete the list");
       } else {
         const data = await response.json();
         return data;
       }
     } catch (error: any) {
-      console.error('Error deleting the list: ', error.message);
+      console.error("Error deleting the list: ", error.message);
     }
   },
 };
