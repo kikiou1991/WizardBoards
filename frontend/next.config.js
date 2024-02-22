a06853645dad43121a6e0f733496750fe600dd6d
kikiou91 <gadorjani@windowslive.com>
2024-02-22 20:49:00 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,20 +1,24 @@
-const {hostname} = require('os');
+const { hostname } = require("os");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'media.istockphoto.com',
+        protocol: "https",
+        hostname: "media.istockphoto.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.google.com',
+        protocol: "https",
+        hostname: "www.google.com",
       },
       {
-        protocol: 'https',
-        hostname: 'trello.com',
+        protocol: "https",
+        hostname: "trello.com",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
       },
     ],
   },
