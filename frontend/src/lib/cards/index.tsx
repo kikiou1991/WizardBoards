a96e9bdc869040ad0624ae4371c9fafec6e561fd
kikiou91 <gadorjani@windowslive.com>
2024-02-22 16:42:46 +0000

    Update index.tsx

--- frontend/src/lib/cards/index.tsx
+++ frontend/src/lib/cards/index.tsx
@@ -29,7 +29,6 @@ export const listCards = {
 
   getCards: async (token: any, listUuid: string) => {
     try {
-      console.log("initiated get cards function");
       const response = await fetch(
         `https://wizardboards.co.uk/api/cards?listUuid=${encodeURIComponent(
           listUuid
