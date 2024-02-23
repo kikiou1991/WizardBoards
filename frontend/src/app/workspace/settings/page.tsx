7e5059976556932fd3f772b518dda9b7918dfbbf
kikiou91 <gadorjani@windowslive.com>
2024-02-23 16:32:44 +0000

    settings update update 1.0.0

--- frontend/src/app/workspace/settings/page.tsx
+++ frontend/src/app/workspace/settings/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+import WorkspaceHeader from "@/components/workspaceHeader";
+import React, { useState } from "react";
 
 const Settings = () => {
+  const [visibility, setVisibility] = useState("Public" as string);
   return (
-    <div>Settings</div>
-  )
-}
+    <div className="flex flex-col flex-wrap overflow-x-hidden overflow-y-auto p-3 gap-2 ">
+      <WorkspaceHeader />
+      <div className="settings">
+        <div>Workspace visibility</div>
+        <div>
+          {visibility} "This Workspace is private. It is not visible to anyone
+          outside of this workspace"
+        </div>
+      </div>
+      <div>
+        <p className="font-bold">Upgrade to premium today</p>{" "}
+        {"for more settings"}
+      </div>
+      <div>
+        <p className="text-red-500">Delete this workspace?</p>
+      </div>
+    </div>
+  );
+};
 
-export default Settings
+export default Settings;
