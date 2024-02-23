309d0ad6ca9f7e3b5dfcb0cf394619f4ec84048a
kikiou91 <gadorjani@windowslive.com>
2024-02-23 19:07:44 +0000

    Update visibilityModal.tsx

--- frontend/src/components/modals/visibilityModal.tsx
+++ frontend/src/components/modals/visibilityModal.tsx
@@ -20,7 +20,7 @@ const VisibilityModal = ({ toggle }: Props) => {
 
       // Calculate the position of the modal relative to the button
       const top = buttonRect.top + buttonRect.height;
-      const left = buttonRect.left + (buttonRect.width - modalWidth);
+      const left = buttonRect.left + (buttonRect.width - modalWidth) * 2;
 
       // Set the modal position
       setModalPosition({ top, left });
