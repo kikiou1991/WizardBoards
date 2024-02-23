e892595716daac1acdb520c07c653f01e667c74b
kikiou91 <gadorjani@windowslive.com>
2024-02-23 17:07:06 +0000

    Update page.tsx

--- frontend/src/app/workspace/settings/page.tsx
+++ frontend/src/app/workspace/settings/page.tsx
@@ -16,21 +16,21 @@ const Settings = () => {
   return (
     <div className="flex flex-col flex-wrap overflow-x-hidden overflow-y-auto p-3 gap-2 ">
       <WorkspaceHeader />
-      <div className="text-foreground ">
+      <div className="text-foreground items-center ">
         <div className="border-b-1 py-4">Workspace visibility</div>
         <div className="flex flex-row ">
-          <div className="flex justify-start py-3">
+          <div className="flex justify-start items-center py-3">
             {visibility} This Workspace is private. It is not visible to anyone
             outside of this workspace
           </div>
-          <Button className="ml-auto">Change</Button>
+          <Button className="ml-auto mr-10">Change</Button>
         </div>
       </div>
-      <div className="upgrade bg-blue-500 w-[900px] h-[400px]">
+      <div className="upgrade bg-slate-400 w-[1200px] h-[400px]">
         <div className="flex flex-row gap-1">
           <p className="font-bold">Upgrade to premium today</p>
           <p>for more settings</p>
-          <Button className="ml-auto">Upgrade</Button>
+          <Button className="ml-auto mr-10">Upgrade</Button>
         </div>
         <div>Some other settings you could do</div>
       </div>
