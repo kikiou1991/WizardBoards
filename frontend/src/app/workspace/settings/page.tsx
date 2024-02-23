2898c05fa215730712b26cb0df80ebacc7b415d0
kikiou91 <gadorjani@windowslive.com>
2024-02-23 17:14:59 +0000

    Update page.tsx

--- frontend/src/app/workspace/settings/page.tsx
+++ frontend/src/app/workspace/settings/page.tsx
@@ -29,7 +29,7 @@ const Settings = () => {
         </div>
       </div>
       <div className="upgrade bg-slate-400 w-[1200px] h-[400px]">
-        <div className="flex flex-row gap-1 items-center">
+        <div className="flex flex-row gap-1 items-center py-3">
           <p className="font-bold">Upgrade to premium today</p>
           <p>for more settings</p>
           <Button className="ml-auto mr-10">Upgrade</Button>
