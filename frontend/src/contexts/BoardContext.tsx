81ad292cbfb7b030d32b93be34d2bac6ab0cea22
kikiou91 <gadorjani@windowslive.com>
2024-02-23 09:13:57 +0000

    Update BoardContext.tsx

--- frontend/src/contexts/BoardContext.tsx
+++ frontend/src/contexts/BoardContext.tsx
@@ -69,7 +69,9 @@ const BoardContextProvider = ({ children }: WorkspaceContextProviderProps) => {
   const [selectedBoard, setSelectedBoard] = useState("");
   const [isBoardSelectedGlobal, setIsBoardSelectedGlobal] = useState(false);
   const [favorites, setFavorites] = useState<Boards[]>([]);
+  console.log("current workspaces: ", workspaces);
   console.log("current boards: ", boards);
+  console.log("current favorites in this workspace: ", favorites);
   //create the boards
   const createBoard = async (token: any, boardData: any) => {
     if (!boardData || !token) return;
