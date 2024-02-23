9f299a8f806eefa82739a77719a4dcf5363a8930
kikiou91 <gadorjani@windowslive.com>
2024-02-23 17:32:14 +0000

    s

--- frontend/src/components/modals/visibilityModal.tsx
+++ frontend/src/components/modals/visibilityModal.tsx
@@ -8,7 +8,7 @@ const VisibilityModal = ({ toggle }: Props) => {
   return (
     <div
       onClick={toggle}
-      className="absolute inset-0 bg-foreground text-background rounded-lg w-[250] h-[250]"
+      className="absolute z-10 bg-foreground text-background rounded-lg w-[250] h-[250]"
     >
       VisibilityModal
     </div>
