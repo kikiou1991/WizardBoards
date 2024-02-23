fede6196aa1b21db0d4832a4e4a839069fa6e73d
kikiou91 <gadorjani@windowslive.com>
2024-02-23 16:58:58 +0000

    Update page.tsx

--- frontend/src/app/workspace/settings/page.tsx
+++ frontend/src/app/workspace/settings/page.tsx
@@ -16,15 +16,15 @@ const Settings = () => {
   return (
     <div className="flex flex-col flex-wrap overflow-x-hidden overflow-y-auto p-3 gap-2 ">
       <WorkspaceHeader />
-      <div className="flex flex-row text-foreground ">
-        <div className="justify-start">
-          <div className="border-b-1 py-4">Workspace visibility</div>
+      <div className="text-foreground ">
+        <div className="border-b-1 py-4">Workspace visibility</div>
+        <div className="flex flex-row justify-start">
           <div className="py-3">
             {visibility} This Workspace is private. It is not visible to anyone
             outside of this workspace
+            <Button className="flex justify-end">Change</Button>
           </div>
         </div>
-        <Button className="flex justify-end">Change</Button>
       </div>
       <div className="upgrade bg-blue-500 w-[900px] h-[400px]">
         <div className="flex flex-row gap-1">
