e7d7a55ef804064659296882e1ebf1e933d5492d
kikiou91 <gadorjani@windowslive.com>
2024-03-19 09:10:32 +0000

    Update index.js

--- backend2/index.js
+++ backend2/index.js
@@ -8,7 +8,11 @@ const { MongoClient } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3002",
+      "https://wizardboards.co.uk",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
