cc7609e1a5c468d03d8d4c4cda63dd94dcdf42cb
kikiou91 <gadorjani@windowslive.com>
2024-03-19 12:17:56 +0000

    Update index.tsx

--- frontend/src/app/actions/index.tsx
+++ frontend/src/app/actions/index.tsx
@@ -2,7 +2,7 @@
 
 import { Resend } from "resend";
 
-const resend = new Resend("re_Y6zgo4Ad_9yN77TuHgtaQUTE3WuFMfLJ7");
+const resend = new Resend(process.env.re_Y6zgo4Ad_9yN77TuHgtaQUTE3WuFMfLJ7);
 
 export const sendEmail = async (formData: any) => {
   const message = formData?.get("message");
